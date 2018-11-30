17014bafac57914ea1aba7c400d59a4fb0787de3
SgfdDttt <nils.holzenberger@laposte.net>
2018-11-30 12:05:47 -0500

    format oja's into prescribed format

--- code/oja.py
+++ code/oja.py
@@ -1,35 +1,39 @@
-import argparse
-import math
+""" Oja's algorithm as described in ... ADD REFERENCE """
 import numpy as np
-from data_streamer import Streamer
-from data_streamer import MultiStreamer
 
-parser = argparse.ArgumentParser(description="Run Oja's algorithm using Numpy")
-parser.add_argument('--data', '-s', type=str,
-                    help='file to be used as data stream')
-parser.add_argument('--n_components', '-k', type=int,
-                    help='number of components of CCA decomposition')
-parser.add_argument('--dimensionality', '-d', type=int,
-                    help='dimensionality of input data')
-parser.add_argument('--savefile', type=str,
-                    help='file to save final transformation to')
-args = parser.parse_args()
-assert args.n_components <= args.dimensionality
-stream = Streamer(args.data).get_stream()
-stream = MultiStreamer([args.data,args.data]).get_stream()
-U = np.eye(args.dimensionality, args.n_components)
-mean = np.zeros(args.dimensionality)
-time = 0
-for point in stream:
-    print(point)
-    print(point[0])
-    assert False
-    time += 1
-    learning_rate = 1./math.sqrt(time)
-    point=np.array(point,dtype=np.float32)
-    mean = ((time - 1.0)*mean + point)/time # update running average
-    point -= mean # center point
-    V = U + learning_rate * np.matmul(np.outer(point,point),U)
-    U, _ = np.linalg.qr(V,mode='reduced')
-print('saving to ' + args.savefile + '...')
-np.save(args.savefile, U)
+def Oja:
+    def __init__(self,hyperparameters):
+        self.hyperparameters=hyperparameters
+        assert 'd' in hyperparameters, 'dimensionality of input vectors not specified'
+        assert 'k' in hyperparameters, 'dimensionality of projection not specified'
+        assert 'learning_rate' in hyperparameters, \
+                'initial learning rate not specified'
+        assert self.hyperparameters['d'] >= self.hyperparameters['k'], \
+                'dimensionality of projection must be smaller than that of original space'
+        self.parameters={
+                'U': np.eye(self.hyperparameters['d'], self.hyperparameters['k']),
+                'mean': np.zeros(self.hyperparameters['d']),
+                't': 0
+                }
+
+    def step(self,point):
+        self.parameters['t'] += 1
+        step_size = self.hyperparameters['learning_rate']**0.5
+        # update running average
+        self.parameters['mean'] = \
+                (self.parameters['t'] - 1.0)*self.parameters['mean'] \
+                + point
+        self.parameters['mean'] /= self.parameters['t'] 
+        point -= self.parameters['mean']
+        gradient = np.matmul(np.outer(point,point),self.parameters['U'])
+        tmp = self.parameters['U'] + step_size * gradient
+        self.parameters['U'], _ = np.linalg.qr(V,mode='reduced')
+
+    def transform(self,points):
+        return np.matmul(points,self.parameters['U'])
+
+    def loss(self,points):
+        uut = np.matmul(self.parameters['U'],self.parameters['U'].T)
+        residuals = points - np.matmul(points,uut)
+        loss = np.sum(np.power(residuals,2))
+        return loss
