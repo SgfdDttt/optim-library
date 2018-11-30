ba0104bc283b3463f7e7ab111629827d375bcbc3
SgfdDttt <nils.holzenberger@laposte.net>
2018-11-30 12:51:47 -0500

    remove logging

--- code/run.py
+++ code/run.py
@@ -25,10 +25,11 @@ def parse_config(config_file_name):
         config[key]=interpret(value)
     return config
 """ END UTIL FUNCTIONS """
-
 assert os.path.isfile(sys.argv[1]), \
         'first and only argument of this script is a config file'
+print('config file: ' + str(sys.argv[1]))
 config=parse_config(sys.argv[1])
+print('config parameters: ' + str(config))
 if len(config['data'].split(','))>1:
     stream = MultiStreamer(config['data'].split(',')).get_stream()
 else:
