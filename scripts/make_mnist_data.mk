c0c46a179caa44ce8657fb141e7ea4fed0442373
SgfdDttt <nils.holzenberger@laposte.net>
2018-11-30 16:26:26 -0500

    rename, txt <- csv

--- scripts/make_mnist_data.mk
+++ scripts/make_mnist_data.mk
@@ -1,11 +1,11 @@
 # inside this directory, run 'make -f make_mnist_data.mk'
-script=scripts/mnist2txt.py
+script=scripts/mnist2csv.py
 
-all: mnist.txt
+all: mnist.csv
 
 train-images-idx3-ubyte:
 	wget yann.lecun.com/exdb/mnist/train-images-idx3-ubyte.gz -O $@.gz
 	gunzip $@.gz
 
-mnist.txt: train-images-idx3-ubyte
+mnist.csv: train-images-idx3-ubyte
 	python $(script) $< $@
