c2f457bc86c8e42e7731f433b473ea4f445ba2dc
SgfdDttt <nils.holzenberger@laposte.net>
2018-11-30 12:43:51 -0500

    add clarifying comments

--- Algorithms/__init__.py
+++ Algorithms/__init__.py
@@ -1,3 +1,6 @@
+# import all our algorithms
 import oja
 
+# left side of equal sign is the name that will be used to describe it in a config file
+# right side of the equal sign is name_of_file.name_of_class
 oja=oja.Oja
