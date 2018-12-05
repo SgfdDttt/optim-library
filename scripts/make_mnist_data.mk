454c107f8641cd44c048973a5df761fde9552632
Nils HOLZENBERGER <nholzenb@orange.fr>
2018-12-05 10:31:23 -0500

    also download the test data

--- scripts/make_mnist_data.mk
+++ scripts/make_mnist_data.mk
@@ -1,11 +1,18 @@
 # inside this directory, run 'make -f make_mnist_data.mk'
 script=mnist2csv.py
 
-all: mnist.csv
+all: train_mnist.csv test_mnist.csv
 
 train-images-idx3-ubyte:
 	wget yann.lecun.com/exdb/mnist/train-images-idx3-ubyte.gz -O $@.gz
 	gunzip $@.gz
 
-mnist.csv: train-images-idx3-ubyte
+t10k-images-idx3-ubyte.gz:
+	wget yann.lecun.com/exdb/mnist/t10k-images-idx3-ubyte.gz -O $@.gz
+	gunzip $@.gz
+
+train_mnist.csv: train-images-idx3-ubyte
+	python $(script) $< $@
+	
+test_mnist.csv: t10k-images-idx3-ubyte.gz
 	python $(script) $< $@
