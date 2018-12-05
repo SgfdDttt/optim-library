5c88fa0f47da4e0b0d061a2598de3d911df3fd04
SgfdDttt <nils.holzenberger@laposte.net>
2018-12-05 11:33:13 -0500

    remove msg import (code does not compile)

--- code/Algorithms/__init__.py
+++ code/Algorithms/__init__.py
@@ -6,8 +6,3 @@ import oja
 # left side of equal sign is the name that will be used to describe it in a config file
 # right side of the equal sign is name_of_file.name_of_class
 oja=oja.Oja
-
-
-import msg
-
-msg = msg.MSG
