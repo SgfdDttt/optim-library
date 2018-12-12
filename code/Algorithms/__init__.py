2142f5ee516b48ad2e3935c9a3e5c9618daa8aca
Nils HOLZENBERGER <nholzenb@orange.fr>
2018-12-12 14:35:37 -0500

    comment out some methods

--- code/Algorithms/__init__.py
+++ code/Algorithms/__init__.py
@@ -3,9 +3,9 @@ sys.path.append(os.getcwd() + '/code/Algorithms')
 # import all our algorithms
 import oja
 import msg_cca
-import minibatch_msg
-import l2_msg
-import rfmsg_cca
+#import minibatch_msg
+#import l2_msg
+#import rfmsg_cca
 
 # left side of equal sign is the name that will be used to describe it in a config file
 # right side of the equal sign is name_of_file.name_of_class
@@ -15,12 +15,12 @@ oja=oja.Oja
 import msg
 msg = msg.MSG
 
-import rfoja
-rfoja = rfoja.RFOja
+#import rfoja
+#rfoja = rfoja.RFOja
 
-minibatch_msg = minibatch_msg.minibatchMSG
-l2_msg = l2_msg.l2MSG
+#minibatch_msg = minibatch_msg.minibatchMSG
+#l2_msg = l2_msg.l2MSG
 
 msg_cca=msg_cca.MSG_CCA
-rfmsg_cca = rfmsg_cca.RFMSG_CCA
+#rfmsg_cca = rfmsg_cca.RFMSG_CCA
 
